5b46f1cf7c6b04fc94a0ad6b68eb64ae32795004
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-24 23:04:38 +0200

    add: authenticatedlink logic

--- src/views/authenticated/AuthenticatedLink.jsx
+++ src/views/authenticated/AuthenticatedLink.jsx
@@ -1,7 +1,22 @@
 import { NavLink } from "react-router";
 import AuthenticatedNavbar from "../../components/AuthenticatedNavbar";
+import { useEffect, useState } from "react";
+import apiClient from "../../apiClient";
+import dayjs from "dayjs";
 
 function AuthenticatedLink() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const userId = localStorage.getItem('userId');
+        fetchData(userId);
+    }, []);
+
+    async function fetchData(userId) {
+        const response = await apiClient.get(`/link/link-details/user/${userId}`);
+        setData(response.data);
+    }
+
     return (
         <>
             <AuthenticatedNavbar />
@@ -29,68 +44,30 @@ function AuthenticatedLink() {
                                         </tr>
                                     </thead>
                                     <tbody className="fw-lighter">
-                                        <tr>
-                                            <td><a href="https://github.com/MlamliLolwane" className="fw-lighter text-white">
-                                                <u>https://github.com/MlamliLolwane</u>
-                                            </a></td>
-                                            <td><a href="https://sht.ly/txldat" className="fw-lighter text-white">
-                                                <u>https://sht.ly/txldat</u>
-                                            </a></td>
-                                            <td>17 March 2025</td>
-                                            <td>17</td>
-                                            <td>
-                                                <div className="dropdown d-lg-none">
-                                                    <button className="btn btn-secondary dropdown-toggle rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                        Options
-                                                    </button>
-                                                    <ul className="dropdown-menu rounded-0">
-                                                        <li><a className="dropdown-item" href="#">DELETE</a></li>
-                                                        <li><a className="dropdown-item" href="#">VIEW</a></li>
-                                                    </ul>
-                                                </div>
-                                                <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline">DELETE</button>
-                                                <NavLink to="/authenticated/links/view" className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
-                                        </tr>
-                                        <tr>
-                                            <td><a href="https://youtube.com" className="fw-lighter text-white">
-                                                <u>https://youtube.com</u></a></td>
-                                            <td><a href="https://sht.ly/gshtat" className="fw-lighter text-white">
-                                                <u>https://sht.ly/gshtat</u></a></td>
-                                            <td>08 April 2025</td>
-                                            <td>6</td>
-                                            <td>
-                                                <div className="dropdown d-lg-none">
-                                                    <button className="btn btn-secondary dropdown-toggle rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                        Options
-                                                    </button>
-                                                    <ul className="dropdown-menu rounded-0">
-                                                        <li><a className="dropdown-item" href="#">DELETE</a></li>
-                                                        <li><a className="dropdown-item" href="#">VIEW</a></li>
-                                                    </ul>
-                                                </div>
-                                                <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline">DELETE</button>
-                                                <NavLink to="/authenticated/links/view" className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
-                                        </tr>
-                                        <tr>
-                                            <td><a href="https://instagram.com" className="fw-lighter text-white">
-                                                <u>https://instagram.com</u></a></td>
-                                            <td><a href="https://sht.ly/thdsta" className="fw-lighter text-white">
-                                                <u>https://sht.ly/thdsta</u></a></td>
-                                            <td>10 June 2025</td>
-                                            <td>24</td>
-                                            <td>
-                                                <div className="dropdown d-lg-none">
-                                                    <button className="btn btn-secondary dropdown-toggle rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                        Options
-                                                    </button>
-                                                    <ul className="dropdown-menu rounded-0">
-                                                        <li><a className="dropdown-item" href="#">DELETE</a></li>
-                                                        <li><a className="dropdown-item" href="#">VIEW</a></li>
-                                                    </ul>
-                                                </div>
-                                                <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline">DELETE</button>
-                                                <NavLink to="/authenticated/links/view" className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
-                                        </tr>
+                                        {data?.map((d) => (
+                                            <tr>
+                                                <td><a href={d.originalUrl} target="_blank" className="fw-lighter text-white">
+                                                    <u>{d.originalUrl}</u>
+                                                </a></td>
+                                                <td><a href={d.shortenedUrl} target="_blank" className="fw-lighter text-white">
+                                                    <u>{d.shortenedUrl}</u>
+                                                </a></td>
+                                                <td>{dayjs(d.createdAt).format("D MMMM, YYYY h:mm A")}</td>
+                                                <td>{d.clickCount}</td>
+                                                <td>
+                                                    <div className="dropdown d-lg-none">
+                                                        <button className="btn btn-secondary dropdown-toggle rounded-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                            Options
+                                                        </button>
+                                                        <ul className="dropdown-menu rounded-0">
+                                                            <li><a className="dropdown-item" href="#">DELETE</a></li>
+                                                            <li><a className="dropdown-item" href="#">VIEW</a></li>
+                                                        </ul>
+                                                    </div>
+                                                    <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline">DELETE</button>
+                                                    <NavLink to="/authenticated/links/view" className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
