385bb9e9963915def8b43cd9ce045842374e3ffe
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-25 22:06:01 +0200

    add: logic for creating links

--- src/views/authenticated/AddLink.jsx
+++ src/views/authenticated/AddLink.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import AuthenticatedNavbar from "../../components/AuthenticatedNavbar";
+import apiClient from "../../apiClient";
+import { useNavigate } from "react-router";
 
 function AddLink() {
+    const [originalUrl, setOriginalUrl] = useState("");
+    const navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        await apiClient.post('/link', {originalUrl: originalUrl, userId: localStorage.getItem('userId')});
+        navigate("/authenticated/links");
+    }
     return (
         <>
             <AuthenticatedNavbar />
@@ -17,13 +28,14 @@ function AddLink() {
                     </div>
                     <div className="row">
                         <div className="col-10">
-                            <form>
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control rounded-0" id="originaLink" />
-                                <label for="originaLink">Enter link to shorten</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">SHORTEN LINK</button>
-                        </form>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-floating mb-3">
+                                    <input type="url" className="form-control rounded-0" id="originalUrl" 
+                                    value={originalUrl} onChange={(e) => setOriginalUrl(e.target.value)} required/>
+                                    <label for="originaLink">Enter link to shorten</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary">SHORTEN LINK</button>
+                            </form>
                         </div>
                     </div>
                 </div>
