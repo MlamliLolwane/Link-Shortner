fc714d35af18c0c6599568d2b72ca18b8ce01455
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-25 22:06:44 +0200

    add: logic to delete links

--- src/views/authenticated/AuthenticatedLink.jsx
+++ src/views/authenticated/AuthenticatedLink.jsx
@@ -17,6 +17,11 @@ function AuthenticatedLink() {
         setData(response.data);
     }
 
+    async function deleteLink(linkId) {
+        await apiClient.delete(`/link/${linkId}`);
+        fetchData(localStorage.getItem('userId'));
+    }
+
     return (
         <>
             <AuthenticatedNavbar />
@@ -36,7 +41,7 @@ function AuthenticatedLink() {
                                 <table className="table table-hover my-4">
                                     <thead>
                                         <tr>
-                                            <th scope="col">Original Link</th>
+                                            <th scope="col" style={{width:"10px"}}>Original Link</th>
                                             <th scope="col">Shortened Link</th>
                                             <th scope="col">Created</th>
                                             <th scope="col">Clicks</th>
@@ -60,12 +65,16 @@ function AuthenticatedLink() {
                                                             Options
                                                         </button>
                                                         <ul className="dropdown-menu rounded-0">
-                                                            <li><a className="dropdown-item" href="#">DELETE</a></li>
-                                                            <li><a className="dropdown-item" href="#">VIEW</a></li>
+                                                            <li><button type="button" className="dropdown-item" onClick={() => deleteLink(d.linkId)}>
+                                                                DELETE</button></li>
+                                                            <li><NavLink className="dropdown-item" 
+                                                            to={`/authenticated/links/view?linkId=${d.linkId}`} >VIEW</NavLink></li>
                                                         </ul>
                                                     </div>
-                                                    <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline">DELETE</button>
-                                                    <NavLink to="/authenticated/links/view" className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
+                                                    <button type="button" className="btn btn-secondary rounded-0 d-none d-lg-inline"
+                                                    onClick={() => deleteLink(d.linkId)}>DELETE</button>
+                                                    <NavLink to={`/authenticated/links/view?linkId=${d.linkId}`}
+                                                    className="btn btn-primary ms-4 d-none d-lg-inline">VIEW</NavLink></td>
                                             </tr>
                                         ))}
                                     </tbody>
