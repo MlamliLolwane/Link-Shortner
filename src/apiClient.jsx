ff6e18fd57d131260b9cde557a929e00d48bdf1b
Mlamli Lolwane <53498225+MlamliLolwane@users.noreply.github.com>
2025-07-10 02:52:48 +0200

    Update apiClient.jsx

--- src/apiClient.jsx
+++ src/apiClient.jsx
@@ -10,7 +10,7 @@ let baseURI = '';
 if (import.meta.env.MODE === 'development') {
   baseURI = 'http://localhost:5080/api/';
 } else if (import.meta.env.MODE === 'production') {
-  baseURI = 'https://lynkly.bigodeals.net'; //
+  baseURI = 'https://lynkly.bigodeals.net/api'; //
 }
 
 const apiClient = axios.create({
